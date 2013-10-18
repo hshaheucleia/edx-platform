1461407554f2af8e1a6207b46cdec495ecfae72d
Adam Palay <adam@edx.org>
2013-10-18 16:12:32 -0400

    first pass at jasmine testing

---
+++ lms/static/js/spec/accessibility_tools_spec.js
@@ -0,0 +1,12 @@
+describe("Tests for accessibility_tools.js", function() {
+
+	describe("Tests for accessible modals"), function() {
+
+		it("is initially aria-hidden"), function() {
+			expect($())
+		}
+
+	};
+
+
+});
